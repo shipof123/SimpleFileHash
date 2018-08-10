ee7177cd8d6401852bd60a421d0588c7a09d901d
shipof123 <shipof123main@gmail.com>
2018-08-09 19:23:48 -0500

    Adding Files

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <bitset>
+
+void hash(std::string& hashed){
+	for(auto ch : hashed){
+		ch ^= (ch >> 7);
+	}
+	for(int ch : hashed){
+		hashed[ch] ^= (hashed[ch+1] ^ >> hashed[ch]);
+	}
+}
+
+int main(int argc, char * argv[]){ 
+	auto file = ifstream(argv[1]).rdbuff();
+	std::cout << file << std::endl;
+	ostringstream o;
+	o << file;
+	std::string hashed = o.str();
+	hash(hashed);
+	unsigned long long int ret(0); 
+	for(auto ch: hashed){
+		ret ^= ch;
+	}
+	return(ret << 4);
+}
